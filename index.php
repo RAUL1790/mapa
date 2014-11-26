66c599fa1d616cf6632aeb0a79e87bd24d75050e
Raul <amos.1790@yahoo.com>
2014-11-25 21:13:08 -0430

    archivos nuevo

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 <html>
 <head>
 	<title>Mapas de Google</title>
+	<link rel="stylesheet" type="text/css" href="">
+	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
 </head>
 <body>
+
 
 </body>
 </html>
