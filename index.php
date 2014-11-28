1e4842c66556f1cf8251f58e937a5123078dd7df
Raul <amos.1790@yahoo.com>
2014-11-27 20:16:55 -0430

    nada nuevo

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
       <script src="http://maps.googleapis.com/maps/api/js"></script>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	  <script type="text/javascript" 
-           src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&sensor=false">
+           src="https://maps.googleapis.com/maps/api/js?v=3.exp">
     </script>
 
     <script type="text/javascript">
@@ -52,6 +52,8 @@
          var marker = new google.maps.Marker({
            position : porlamar,
            map : map,
+           title:"Hogar",
+           animation: google.maps.Animation.DROP
          });     
 
 
@@ -69,7 +71,7 @@
 <body>
 <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d250694.4939711694!2d-64.09429219999996!3d10.955707450000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c31932a585cfef3%3A0x3c082f2200cab1c0!2sNueva+Esparta!5e0!3m2!1ses!2sve!4v1416967491860" width="600" height="450" frameborder="0" style="border:0"></iframe>-->
 
-<div id="map_canvas"></div>
+<div id="map_canvas" ></div>
 
 </body>
 </html>
