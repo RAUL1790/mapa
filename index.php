077a4cb5b1038d31bc3c52f4ab48aea82076f89f
Raul <amos.1790@yahoo.com>
2014-11-27 21:40:51 -0430

    se agrego contenido al marker

--- index.php
+++ index.php
@@ -23,11 +23,7 @@
     </script>
 
     <script type="text/javascript">
-       
-     var map; 
-     var service;
-
-
+        
 
        function inicializar(locacion)
        {
@@ -42,33 +38,41 @@
                mapTypeId: google.maps.MapTypeId.ROADMAP
             }
 
-            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+           var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
 
           /*           PONER MARCADORES             */
           
          var porlamar =  new google.maps.LatLng(10.973707, -63.875057);
 
-         var marker = new google.maps.Marker({
+          var marker1 = new google.maps.Marker({
            position : porlamar,
            map : map,
            title:"Hogar",
            animation: google.maps.Animation.DROP
-         });     
-
-
-
-       } // FIN FUNCION INICIALIZAR
+         });    
+  
       
-      $(document).ready(function()
+           var informacion = new google.maps.InfoWindow({
+               content: "aqui esta mi hogar"
+           });
+
+           google.maps.event.addListener(marker1, 'click', function() {
+               informacion.open(map,marker1);
+          });
+
+
+     } // FIN FUNCION INICIALIZAR
+
+    /*  $(document).ready(function()
       {
         // navigator.geolocation.getCurrentPosition(inicializar);   con esto puedo obtener la locacion donde me encuentro      
        inicializar();
-      });    
+      }); */   
     </script>
 
 </head>
-<body>
+<body onload="inicializar()">
 <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d250694.4939711694!2d-64.09429219999996!3d10.955707450000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c31932a585cfef3%3A0x3c082f2200cab1c0!2sNueva+Esparta!5e0!3m2!1ses!2sve!4v1416967491860" width="600" height="450" frameborder="0" style="border:0"></iframe>-->
 
 <div id="map_canvas" ></div>
