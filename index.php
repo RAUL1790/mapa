204e36455c3c24d0726756bb88af3b5bc7675e31
Raul <amos.1790@yahoo.com>
2014-11-27 19:52:14 -0430

    se agrego marcador al mapa

--- index.php
+++ index.php
@@ -10,43 +10,58 @@
      <!--        GOOGLE MAPS                    -->
 
       <style type="text/css">
-      #map_canvas { width:500px; height: 500px }
+      #map_canvas { width:1000px; height: 1000px}
     </style>
 
 
      </style>
 
       <script src="http://maps.googleapis.com/maps/api/js"></script>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"</script>
-	  <script type="text/javascript"
-      src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCFbVVm2OkmvZOSPUMF5uXaOukaIlD0TXk&sensor=false">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	  <script type="text/javascript" 
+           src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&sensor=false">
     </script>
 
     <script type="text/javascript">
        
-      
-      
+     var map; 
+     var service;
+
+
 
        function inicializar(locacion)
        {
        /* console.log(locacion); 
         console.log(locacion.coords.latitude)  
         console.log(locacion.coords.longitude)*/ 
+        var nueva_esparta =  new google.maps.LatLng(10.961573, -64.096157);
            var mapOptions =
             {
-              zoom: 8,
-              center: new google.maps.LatLng(locacion.coords.latitude, locacion.coords.longitude),
+               zoom: 11,
+               center: nueva_esparta,
                mapTypeId: google.maps.MapTypeId.ROADMAP
             }
 
-         var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-     
-       }
+            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+
+          /*           PONER MARCADORES             */
+          
+         var porlamar =  new google.maps.LatLng(10.973707, -63.875057);
+
+         var marker = new google.maps.Marker({
+           position : porlamar,
+           map : map,
+         });     
+
+
+
+       } // FIN FUNCION INICIALIZAR
       
       $(document).ready(function()
       {
-        navigator.geolocation.getCurrentPosition(inicializar);        
-
+        // navigator.geolocation.getCurrentPosition(inicializar);   con esto puedo obtener la locacion donde me encuentro      
+       inicializar();
       });    
     </script>
 
